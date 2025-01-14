cf8261642dc985489d208a09dce7875a48af7796
wonsunahn <54322243+wonsunahn@users.noreply.github.com>
2025-01-14 15:42:11 -0500

    Added cat scale exercise

--- src/main/java/edu/pitt/cs/CatScale.java
+++ src/main/java/edu/pitt/cs/CatScale.java
@@ -3,7 +3,7 @@ package edu.pitt.cs;
 //TODO: Import libraries as needed
 import java.util.NoSuchElementException;
 
-public class SortedCollection {
+public class CatScale {
 	// TODO: Add member variables or methods as needed
 
 	/**
@@ -29,28 +29,22 @@ public class SortedCollection {
 	}
 
 	/**
-	 * Prints usage information.
-	 */
-	public static void showUsage() {
-		System.out.println("Usage: java SortedCollection [num1] [num2] [num3] ...");
-	}
-
-	/**
 	 * Main method. Receives a list of numbers as commandline arguments and prints
 	 * out the list in sorted order from smallest to largest.
 	 * 
 	 * @param args commandline arguments; see showUsage() for detailed information
 	 */
 	public static void main(String[] args) {
-		SortedCollection collection = new SortedCollection();
-		if (args.length == 0) {
-			showUsage();
+		CatScale collection = new CatScale();
+
+		try {
+			for (int i = 0; i < args.length; i++) {
+				int num = Integer.parseInt(args[i]);
+				collection.add(num);
+			}
+		} catch (NumberFormatException ne) {
 			return;
 		}
-		
-		// TODO: add numbers in commandline arguments to collection using the add(int) method.
-		// If any commandline argument is not a number, call showUsage() and return.
-		
 		System.out.print("sorted: ");
 		for (int i = 0; i < args.length; i++) {
 			int num = collection.remove();
